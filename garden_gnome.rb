e1ae75a789f6196650d1cd44f5d626e061ef837c
salehlayaly <lsaleh@hcstonline.org>
2018-08-16 03:22:47 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,1 +1,32 @@
 # Code your instances here
+class GardenGnome 
+  def name 
+    @name 
+  end 
+  
+  def name= (new_name) 
+    @name = new_name 
+  end  
+  
+  def age
+    @age 
+  end 
+  
+  def age= (new_age)
+    @age = new_age
+  end    
+  def gluten_allergy 
+    @gluten_allergy 
+  end 
+  
+  def gluten_allergy= (new_gluten_allergy) 
+    @gluten_allergy = new_gluten_allergy
+  end 
+  
+  def personality
+    @personality = "evil"
+end
+    
+    
+    
+
